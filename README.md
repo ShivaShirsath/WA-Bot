5017d49e49914473497c677680b8dd5f8cef07fb
Shiva Shirsath <Shiva.S.Shirsath@gmail.com>
2023-03-09 23:33:12 +0530

    deploy

---
+++ README.md
@@ -0,0 +1,55 @@
+# Whatsapp-Chat-Bot
+
+**_WhatsApp OpenAI Create with NodeJS Using Library [Baileys](https://github.com/adiwajshing/Baileys)_**
+
+## BACA DULU
+
+Terdapat 2 fitur yaitu `ChatGPT(text)` dan `DALL-E(Text To Image).`<br>Untuk commandnya menggunakan `/ai` dan `/img`
+
+## Get & Change OpenAI ApiKey
+
+- Silakan buat apikeynya terlebih dahulu [Disini](https://beta.openai.com/account/api-keys)
+- Ganti ApiKey OpenAI pada file [key.json](https://github.com/ShivaShirsath/Whatsapp-Chat-Bot/blob/844f41c362250bed10857443c013cbb454807e87/key.json#L2)
+
+## Install
+
+**Install on RDP/Windows ✅**
+
+Install [NodeJS](https://nodejs.org/en/download/)
+dan [Git Bash](https://git-scm.com/downloads) terlebih dahulu
+
+```bash
+$ git clone https://github.com/ShivaShirsath/Whatsapp-Chat-Bot
+$ cd Whatsapp-Chat-Bot
+$ npm install
+$ node index.js
+```
+
+**Install on Termux ✅**
+
+Silakan install manual `node_modules` terlebih dahulu, karena di termux saat install otomatis melalui `npm install` terjadi error.
+<br>Link `node_modules`: [Klik Disini](https://drive.google.com/file/d/1xgRIwDVuTklxwdtsx933WfmzqtRxEsGV/view?usp=share_link)
+
+```bash
+$ pkg install git nodejs -y
+$ git clone https://github.com/ShivaShirsath/Whatsapp-Chat-Bot
+$ cd Whatsapp-Chat-Bot
+```
+
+Sesudah menginstall file node_modules, silahkan extract file `node_modules.zip` dan taruh di folder Whatsapp-Chat-Bot.
+
+**Run**
+
+```bash
+$ node index.js
+```
+
+## Donate
+
+<a href="https://github.com/ShivaShirsath" target="_blank"><img src="https://user-images.githubusercontent.com/26188697/180601310-e82c63e4-412b-4c36-b7b5-7ba713c80380.png" alt="Donate For Shiva" height="41" width="174"></a>
+
+## License
+
+[MIT License](https://github.com/ShivaShirsath/Whatsapp-Chat-Bot/blob/main/LICENSE)
+
+Copyright (c) 2022 M Shiva
---
+++ shiva.js
@@ -0,0 +1,237 @@
+const {
+  BufferJSON,
+  WA_DEFAULT_EPHEMERAL,
+  generateWAMessageFromContent,
+  proto,
+  generateWAMessageContent,
+  generateWAMessage,
+  prepareWAMessageMedia,
+  areJidsSameUser,
+  getContentType,
+} = require("@adiwajshing/baileys");
+const fs = require("fs");
+const util = require("util");
+const chalk = require("chalk");
+const { Configuration, OpenAIApi } = require("openai");
+let setting = require("./key.json");
+
+module.exports = shiva = async (client, m, chatUpdate, store) => {
+  try {
+    var body =
+      m.mtype === "conversation"
+        ? m.message.conversation
+        : m.mtype == "imageMessage"
+        ? m.message.imageMessage.caption
+        : m.mtype == "videoMessage"
+        ? m.message.videoMessage.caption
+        : m.mtype == "extendedTextMessage"
+        ? m.message.extendedTextMessage.text
+        : m.mtype == "buttonsResponseMessage"
+        ? m.message.buttonsResponseMessage.selectedButtonId
+        : m.mtype == "listResponseMessage"
+        ? m.message.listResponseMessage.singleSelectReply.selectedRowId
+        : m.mtype == "templateButtonReplyMessage"
+        ? m.message.templateButtonReplyMessage.selectedId
+        : m.mtype === "messageContextInfo"
+        ? m.message.buttonsResponseMessage?.selectedButtonId ||
+          m.message.listResponseMessage?.singleSelectReply.selectedRowId ||
+          m.text
+        : "";
+    var budy = typeof m.text == "string" ? m.text : "";
+    // var prefix = /^[\\/!#.]/gi.test(body) ? body.match(/^[\\/!#.]/gi) : "/"
+    var prefix = /^[\\/!#.]/gi.test(body) ? body.match(/^[\\/!#.]/gi) : "/";
+    const isCmd2 = body.startsWith(prefix);
+    const command = body
+      .replace(prefix, "")
+      .trim()
+      .split(/ +/)
+      .shift()
+      .toLowerCase();
+    const args = body.trim().split(/ +/).slice(1);
+    const pushname = m.pushName || "No Name";
+    const botNumber = await client.decodeJid(client.user.id);
+    const itsMe = m.sender == botNumber ? true : false;
+    let text = (q = args.join(" "));
+    const arg = budy.trim().substring(budy.indexOf(" ") + 1);
+    const arg1 = arg.trim().substring(arg.indexOf(" ") + 1);
+
+    const from = m.chat;
+    const reply = m.reply;
+    const sender = m.sender;
+    const mek = chatUpdate.messages[0];
+
+    const color = (text, color) => {
+      return !color ? chalk.green(text) : chalk.keyword(color)(text);
+    };
+
+    // Group
+    const groupMetadata = m.isGroup
+      ? await client.groupMetadata(m.chat).catch((e) => {})
+      : "";
+    const groupName = m.isGroup ? groupMetadata.subject : "";
+
+    // Push Message To Console
+    let argsLog = budy.length > 30 ? `${q.substring(0, 30)}...` : budy;
+
+    if (isCmd2 && !m.isGroup) {
+      console.log(
+        chalk.black(chalk.bgWhite("[ LOGS ]")),
+        color(argsLog, "turquoise"),
+        chalk.magenta("From"),
+        chalk.green(pushname),
+        chalk.yellow(`[ ${m.sender.replace("@s.whatsapp.net", "")} ]`)
+      );
+    } else if (isCmd2 && m.isGroup) {
+      console.log(
+        chalk.black(chalk.bgWhite("[ LOGS ]")),
+        color(argsLog, "turquoise"),
+        chalk.magenta("From"),
+        chalk.green(pushname),
+        chalk.yellow(`[ ${m.sender.replace("@s.whatsapp.net", "")} ]`),
+        chalk.blueBright("IN"),
+        chalk.green(groupName)
+      );
+    }
+
+    if (isCmd2) {
+      switch (command) {
+        case "💝":
+          m.reply(`
+*Whatsapp Bot OpenAI*
+            
+  *Chat GPT*
+    Command: ${prefix}💛
+    Ask anything to AI. 
+
+  *DALL-E*
+    Command: ${prefix}💖
+    Create an image from text
+`);
+          break;
+        case "💛":
+          try {
+            if (setting.keyopenai === "ISI_APIKEY_OPENAI_DISINI")
+              return reply(
+                `
+The api key has not been filled.
+
+Please fill in the api key first in the key.json file
+
+The api key can be generated on the website:
+https://beta.openai.com/account/api-keys
+`
+              );
+            if (!text)
+              return reply(
+                `Chat with AI.\n\nExample:\n${prefix}${command} What is a recession`
+              );
+            const configuration = new Configuration({
+              apiKey: setting.keyopenai,
+            });
+            const openai = new OpenAIApi(configuration);
+
+            const response = await openai.createCompletion({
+              model: "text-davinci-003",
+              prompt: text,
+              temperature: 0, // Higher values means the model will take more risks.
+              max_tokens: 2048, // The maximum number of tokens to generate in the completion. Most models have a context length of 2048 tokens (except for the newest models, which support 4096).
+              top_p: 1, // alternative to sampling with temperature, called nucleus sampling
+              frequency_penalty: 0.3, // Number between -2.0 and 2.0. Positive values penalize new tokens based on their existing frequency in the text so far, decreasing the model's likelihood to repeat the same line verbatim.
+              presence_penalty: 0, // Number between -2.0 and 2.0. Positive values penalize new tokens based on whether they appear in the text so far, increasing the model's likelihood to talk about new topics.
+            });
+            m.reply(`${response.data.choices[0].text}`);
+          } catch (error) {
+            if (error.response) {
+              console.log(error.response.status);
+              console.log(error.response.data);
+              console.log(`${error.response.status}\n\n${error.response.data}`);
+            } else {
+              console.log(error);
+              m.reply("Sorry, there seems to be an error:" + error.message);
+            }
+          }
+          break;
+        case "💖":
+        case "image":
+        case "images":
+          try {
+            if (setting.keyopenai === "ISI_APIKEY_OPENAI_DISINI")
+              return reply(
+                `
+The api key has not been filled.
+
+Please fill in the api key first in the key.json file
+
+The api key can be generated on the website:
+https://beta.openai.com/account/api-keys
+`
+              );
+            if (!text)
+              return reply(
+                `
+Create an image from AI.
+
+Example:
+  ${prefix}${command} Wooden house on snow mountain
+`
+              );
+            const configuration = new Configuration({
+              apiKey: setting.keyopenai,
+            });
+            const openai = new OpenAIApi(configuration);
+            const response = await openai.createImage({
+              prompt: text,
+              n: 1,
+              size: "512x512",
+            });
+            //console.log(response.data.data[0].url)
+            client.sendImage(from, response.data.data[0].url, text, mek);
+          } catch (error) {
+            if (error.response) {
+              console.log(error.response.status);
+              console.log(error.response.data);
+              console.log(`${error.response.status}\n\n${error.response.data}`);
+            } else {
+              console.log(error);
+              m.reply("Sorry, there seems to be an error:" + error.message);
+            }
+          }
+          break;
+        default: {
+          if (isCmd2 && budy.toLowerCase() != undefined) {
+            if (m.chat.endsWith("broadcast")) return;
+            if (m.isBaileys) return;
+            if (!budy.toLowerCase()) return;
+            if (argsLog || (isCmd2 && !m.isGroup)) {
+              // client.sendReadReceipt(m.chat, m.sender, [m.key.id])
+              console.log(
+                chalk.black(chalk.bgRed("[ ERROR ]")),
+                color("command", "turquoise"),
+                color(`${prefix}${command}`, "turquoise"),
+                color("not available", "turquoise")
+              );
+            } else if (argsLog || (isCmd2 && m.isGroup)) {
+              // client.sendReadReceipt(m.chat, m.sender, [m.key.id])
+              console.log(
+                chalk.black(chalk.bgRed("[ ERROR ]")),
+                color("command", "turquoise"),
+                color(`${prefix}${command}`, "turquoise"),
+                color("not available", "turquoise")
+              );
+            }
+          }
+        }
+      }
+    }
+  } catch (err) {
+    m.reply(util.format(err));
+  }
+};
+
+let file = require.resolve(__filename);
+fs.watchFile(file, () => {
+  fs.unwatchFile(file);
+  console.log(chalk.redBright(`Update ${__filename}`));
+  delete require.cache[file];
+  require(file);
+});
